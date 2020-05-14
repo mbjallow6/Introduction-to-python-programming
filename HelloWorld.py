12a51b1ee88ab215c6c61d32091d38154f445fef
mbjallow6 <mbjallow6@gmail.com>
2020-05-15 01:05:08 +0800

    python Hello World program

---
+++ HelloWorld.py
@@ -0,0 +1,4 @@
+# This my first python program
+# I wrote a hashtag at the beginning to make sure this line is seen as a comment by the interpreter
+name = "Luka"
+print ("Hello I am luka %s your introduction to python instructor" %name)
